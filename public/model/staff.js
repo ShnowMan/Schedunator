65c7b13fd57f97b05740ffb5954cb655739362cd
Jeff King <king.jeffey@gmail.com>
2016-11-06 16:10:58 -0800

    got new employee function reciving data from form

--- public/model/staff.js
+++ public/model/staff.js
@@ -1,20 +1,36 @@
 (function(ctx){
   const Staff = {};
+
   Staff.allEmployees = [];
+
+  Staff.e = null;
 
   const employee = function(args){
     this.name = args[0];
     this.ent = args[1];
     this.ext = args[2];
     this.tp = args[3];
-  }
+    Staff.allEmployees.push(this);
+  };
 
   Staff.addEmployees = function(event){
     event.preventDefault();
-    console.log(event);
-  }
+    Staff.e = event;
+    const args = [
+      Staff.e.currentTarget.name.value,
+      $('input[name="BflyEnt"]:checked').val(),
+      $('input[name="BflyExt"]:checked').val(),
+      $('input[name="tidePool"]:checked').val()
+    ];
+
+    Employee = new employee(args);
+    console.log(Staff.allEmployees);
+  };
 
   $('#WarCard').on("submit",Staff.addEmployees)
-
   ctx.Staff = Staff;
 })(window)
+
+
+// $('input[name="BflyEnt"]:checked').val()
+// Staff.e.currentTarget.name.value
